1644af7dffbeeaf1cfd7d3ddda6f5fe6f8ee9a31
RamziHADHRI <ramzi.hadri@gmail.com>
2021-06-06 16:44:15 +0200

    add files

---
+++ src/main/java/com/example/ramzihadhri/RamzihadhriApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramzihadhri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamzihadhriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamzihadhriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ramzihadhri/RamzihadhriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ramzihadhri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamzihadhriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
